4fbe86e74ffa9a7739b8d948f5c1a565aa6a9288
liwei2151284 <liwei2151284@163.com>
2025-08-07 12:59:32 +0800

    Initial commit

---
+++ jfbuild.sh
@@ -0,0 +1,6 @@
+jf c add demo  --url "https://soleng.jfrog.io" --access-token   --interactive=false
+jf gradlec --global --repo-deploy reponame --repo-resolve reponame
+jf gradle clean artifactoryPublish -b build.gradle  --build-name buildname --build-number 1.0.2
+jf rt bce buildname 1.0.2
+jf rt bp buildname 1.0.2
+jf rt bs buildname 1.0.2 --fail=false
